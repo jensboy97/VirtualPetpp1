fecb69b80f080d45909c08de25ce05c9fd89f38e
jensboy97 <141591601+jensboy97@users.noreply.github.com>
2023-11-24 15:03:58 +0100

    Add files via upload

---
+++ virtuelPet/ConsoleApp1/ConsoleApp1/Class1.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+using ConsoleApp1;
+namespace ConsoleApp1;
+
+
+    internal class character
+    {
+        public int petId;
+        public string name;
+
+         public int age;
+
+        public string gender;
+        public character(int petId, string name, int age, string gender)
+        {
+            this.petId = petId;
+            this.name = name;
+            this.age = age;
+            this.gender = gender; 
+        }
+
+        public character(string name)
+        {
+            this.name = name;
+        }
+
+
+    }
+
---
+++ virtuelPet/ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,95 @@
+﻿using System.Reflection.Metadata.Ecma335;
+using ConsoleApp1;
+
+
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+
+        public static List<character> list = new List<character>();
+        public static int index;
+        static void Main(string[] args)
+        {
+            list.Add(new character(0,"Bulbasaur", 3, "Dame"));
+            list.Add(new character(1,"Kala", 5, "Dame"));
+            list.Add(new character(2,"Fido", 19, "Mann"));
+            
+            
+            choosePet();
+
+        }
+
+        public static void choosePet()
+        {
+            Console.WriteLine("Velg ditt dyr med å taste inn Iden");
+            for (int i=0; i < list.Count; i++)
+            {
+                var character = list[i];
+                Console.WriteLine("petId:" + " " + i + " " + "Navn:" + " " + character.name + " " + "Alder:" + " " + character.age + "år" + " " + "Kjønn:" + " " + character.gender); }
+            index = Convert.ToInt32(Console.ReadLine());
+          Console.WriteLine(list[index].name);
+          Menu();
+        }
+        //public static void newPet()
+        //{
+        //    int newId = Convert.ToInt32(Console.ReadLine());
+        //    string newName = Console.ReadLine();
+        //    int newAge = Convert.ToInt32(Console.ReadLine());
+        //    string newGender = Console.ReadLine();
+           
+        //   list.Add(new character(newId, newName, newAge, newGender));
+           
+        //}
+
+        public static void Menu()
+        {
+            
+            //Console.Clear();
+            Say("1", $"Gi {list[index].name} mat");
+            Say("2", $"Kos med {list[index].name}");
+            Say("3", $"Sjekk om {list[index].name} må på do");
+            Say("4", $"Har {list[index].name} lyst til å kose? ");
+            Say("5", "choose new pet");
+            string option = Console.ReadLine();
+
+            if (option == "1")
+            {
+                Console.WriteLine($"{ list[index].name} er mett, fornøyd og har et smil rundt munnen!");
+            }
+            else if (option == "2")
+            {
+                Console.WriteLine($"Du og { list[index].name} har fått et sterkere bånd. Bulbasaur elsker kos, og slikker deg i ansiktet!");
+            }
+            else if (option == "3")
+            {
+                Console.WriteLine($"{ list[index].name} bjeffer, klynker og klatrer opp på deg om å få gå på do! {list[index].name} må på do med engang!");
+            }
+            else if (option == "4")
+            {
+                Console.WriteLine("Nei");
+            }
+            else if (option == "5")
+            {
+                choosePet();
+            }
+            else
+            {
+                Console.WriteLine($"Error! {list[index].name} is now dead");
+                list.Remove(list[index]);
+                choosePet();
+            }
+
+            
+            Menu();
+        }
+        static void Say(string prefix, string message)
+        {
+            Console.Write("[");
+            Console.Write(prefix);
+            Console.WriteLine("]" + message);
+        }
+    }
+}
+    
